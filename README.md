233e8fc8ad38599188c97bffbac37f011eed654f
geeksmarter <andrewalsante@gmail.com>
2015-10-05 03:11:56 -0400

    Uploading project

---
+++ README.md
@@ -0,0 +1,2 @@
+#Jam
+I'm in a jam with Angular-Fullstack, so I'm going with it's simpler version until I can get my shit together.
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jamApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jamApp
+ */
+angular.module('jamApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
